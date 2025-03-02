abeae8c7bb4d36a6ac9cc3083930c45029cdddfe
Michael Oliver <michael.oliver_@outlook.com>
2025-03-02 13:47:30 +0000

    feat: add command input interface sp (#5)

--- src/game/sp_main.cpp
+++ src/game/sp_main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <vector>
 
 #include "sp_main.h"
 #include "sp_structs.h"
@@ -7,9 +8,80 @@
 #include "../filesystem.h"
 #include "../xboxkrnl.h"
 
+namespace
+{
+    uint32_t ShowKeyboard(const wchar_t *title, const wchar_t *description, const wchar_t *defaultText, std::string &result, size_t maxLength, uint32_t keyboardType)
+    {
+        size_t realMaxLength = maxLength + 1;
+        XOVERLAPPED overlapped = {};
+
+        std::vector<wchar_t> wideBuffer(realMaxLength);
+        std::vector<char> buffer(realMaxLength);
+
+        XShowKeyboardUI(0, keyboardType, defaultText, title, description, wideBuffer.data(), realMaxLength, &overlapped);
+
+        while (!XHasOverlappedIoCompleted(&overlapped))
+            Sleep(100);
+
+        if (XGetOverlappedResult(&overlapped, nullptr, TRUE) == ERROR_SUCCESS)
+        {
+            wcstombs_s(nullptr, buffer.data(), realMaxLength, wideBuffer.data(), realMaxLength * sizeof(wchar_t));
+            result = buffer.data();
+            return ERROR_SUCCESS;
+        }
+
+        return ERROR_CANCELLED;
+    }
+}
+
 namespace sp
 {
+    // Functions
+    Cbuf_AddText_t Cbuf_AddText = reinterpret_cast<Cbuf_AddText_t>(0x821DE9D8);
+
+    CL_ConsolePrint_t CL_ConsolePrint = reinterpret_cast<CL_ConsolePrint_t>(0x822D7040);
+    CL_GamepadButtonEvent_t CL_GamepadButtonEvent = reinterpret_cast<CL_GamepadButtonEvent_t>(0x822CE630);
+
+    Cmd_AddCommandInternal_t Cmd_AddCommandInternal = reinterpret_cast<Cmd_AddCommandInternal_t>(0x821DFAD0);
+
+    Com_Printf_t Com_Printf = reinterpret_cast<Com_Printf_t>(0x821DC0A0);
+
+    Scr_AddSourceBuffer_t Scr_AddSourceBuffer = reinterpret_cast<Scr_AddSourceBuffer_t>(0x821C5A18);
     Scr_ReadFile_FastFile_t Scr_ReadFile_FastFile = reinterpret_cast<Scr_ReadFile_FastFile_t>(0x821C5978);
+
+    // Variables
+    auto cmd_functions = reinterpret_cast<cmd_function_s *>(0x82DDEFCC);
+
+    Detour CL_ConsolePrint_Detour;
+
+    void CL_ConsolePrint_Hook(int localClientNum, int channel, const char *txt, int duration, int pixelWidth, int flags)
+    {
+        CL_ConsolePrint_Detour.GetOriginal<decltype(CL_ConsolePrint)>()(localClientNum, channel, txt, duration, pixelWidth, flags);
+        xbox::DbgPrint("CL_ConsolePrint txt=%s \n", txt);
+    }
+
+    Detour CL_GamepadButtonEvent_Detour;
+
+    void CL_GamepadButtonEvent_Hook(int localClientNum, int controllerIndex, int key, int down, unsigned int time)
+    {
+        xbox::DbgPrint("CL_GamepadButtonEvent localClientNum=%d controllerIndex=%d key=%d down=%d time=%d\n", localClientNum, controllerIndex, key, down, time);
+        CL_GamepadButtonEvent_Detour.GetOriginal<decltype(CL_GamepadButtonEvent)>()(localClientNum, controllerIndex, key, down, time);
+
+        if (key == K_BUTTON_RSTICK && down)
+        {
+            std::string value;
+            auto result = ShowKeyboard(L"Title", L"Description", L"Default Text", value, 100, 0);
+            if (result == ERROR_SUCCESS)
+            {
+                xbox::DbgPrint("ShowKeyboard result: %s\n", value.c_str());
+                Cbuf_AddText(0, value.c_str());
+            }
+            else
+            {
+                xbox::DbgPrint("ShowKeyboard cancelled.\n");
+            }
+        }
+    }
 
     Detour Scr_ReadFile_FastFile_Detour;
 
@@ -62,11 +134,26 @@ namespace sp
         return contents;
     }
 
+    void Cmd_test_f()
+    {
+        xbox::DbgPrint("test command\n");
+    }
+
     void init()
     {
-        xbox::DbgPrint("Initializing SP logic...");
+        xbox::DbgPrint("Initializing SP\n");
+
+        CL_ConsolePrint_Detour = Detour(CL_ConsolePrint, CL_ConsolePrint_Hook);
+        CL_ConsolePrint_Detour.Install();
+
+        CL_GamepadButtonEvent_Detour = Detour(CL_GamepadButtonEvent, CL_GamepadButtonEvent_Hook);
+        CL_GamepadButtonEvent_Detour.Install();
 
         Scr_ReadFile_FastFile_Detour = Detour(Scr_ReadFile_FastFile, Scr_ReadFile_FastFile_Hook);
         Scr_ReadFile_FastFile_Detour.Install();
+
+        cmd_function_s *test_cmd = new cmd_function_s;
+
+        Cmd_AddCommandInternal("test", Cmd_test_f, test_cmd);
     }
 }
