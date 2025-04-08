7e6421d8b7c9bbf06854f69b24ade48b34ec22c4
Michael Oliver <michael.oliver_@outlook.com>
2025-04-08 15:33:04 +0100

    add cmd `dump_images`

--- src/game/iw3-328/cg_consolecmds.cpp
+++ src/game/iw3-328/cg_consolecmds.cpp
@@ -1,6 +1,10 @@
 #pragma warning(push)
 #pragma warning(disable : 4480)
 
+#include <xtl.h>
+#include <xgraphics.h>
+#include <fstream>
+
 #include <algorithm>
 
 #include "..\..\detour.h"
@@ -60,6 +64,59 @@ namespace iw3_328
     typedef void (*Com_PrintError_t)(conChannel_t channel, const char *fmt, ...);
     Com_PrintError_t Com_PrintError = reinterpret_cast<Com_PrintError_t>(0x821FFFA0);
 
+    enum MapType : __int32
+    {
+        MAPTYPE_NONE = 0x0,
+        MAPTYPE_INVALID1 = 0x1,
+        MAPTYPE_INVALID2 = 0x2,
+        MAPTYPE_2D = 0x3,
+        MAPTYPE_3D = 0x4,
+        MAPTYPE_CUBE = 0x5,
+        MAPTYPE_COUNT = 0x6,
+    };
+
+    struct CardMemory
+    {
+        int platform[1];
+    };
+
+    struct GfxImageLoadDef; // Forward declaration
+
+    union GfxTexture
+    {
+        D3DBaseTexture *basemap;
+        D3DTexture *map;
+        D3DVolumeTexture *volmap;
+        D3DCubeTexture *cubemap;
+        GfxImageLoadDef *loadDef;
+    };
+
+    struct GfxImageLoadDef
+    {
+        unsigned __int8 levelCount;
+        unsigned __int8 flags;
+        __int16 dimensions[3];
+        int format;
+        GfxTexture texture;
+    };
+
+    struct GfxImage
+    {
+        MapType mapType;
+        GfxTexture texture;
+        unsigned __int8 semantic;
+        CardMemory cardMemory;
+        unsigned __int16 width;
+        unsigned __int16 height;
+        unsigned __int16 depth;
+        unsigned __int8 category;
+        unsigned __int8 *pixels;
+        unsigned int baseSize;
+        unsigned __int16 streamSlot;
+        bool streaming;
+        const char *name;
+    };
+
     struct RawFile
     {
         const char *name;
@@ -117,7 +174,7 @@ namespace iw3_328
         // MaterialPixelShader *pixelShader;
         // MaterialVertexShader *vertexShader;
         // MaterialTechniqueSet *techniqueSet;
-        // GfxImage *image;
+        GfxImage *image;
         // snd_alias_list_t *sound;
         // SndCurve *sndCurve;
         // clipMap_t *clipMap;
@@ -250,6 +307,327 @@ namespace iw3_328
         Com_Printf(CON_CHANNEL_CONSOLEONLY, "Dumped map ents to 'game:\\_iw3xe\\dump\\%s'\n", cm->mapEnts->numEntityChars, asset_name.c_str());
     }
 
+    struct DDSHeader
+    {
+        uint32_t magic;
+        uint32_t size;
+        uint32_t flags;
+        uint32_t height;
+        uint32_t width;
+        uint32_t pitchOrLinearSize;
+        uint32_t depth;
+        uint32_t mipMapCount;
+        uint32_t reserved1[11];
+        struct
+        {
+            uint32_t size;
+            uint32_t flags;
+            uint32_t fourCC;
+            uint32_t rgbBitCount;
+            uint32_t rBitMask;
+            uint32_t gBitMask;
+            uint32_t bBitMask;
+            uint32_t aBitMask;
+        } pixelFormat;
+        uint32_t caps;
+        uint32_t caps2;
+        uint32_t caps3;
+        uint32_t caps4;
+        uint32_t reserved2;
+    };
+
+    static_assert(sizeof(DDSHeader) == 128, "");
+
+    const uint32_t DDS_MAGIC = MAKEFOURCC('D', 'D', 'S', ' ');
+    const uint32_t DDS_HEADER_SIZE = 124;
+    const uint32_t DDS_PIXEL_FORMAT_SIZE = 32;
+    const uint32_t DDSD_CAPS = 0x1;
+    const uint32_t DDSD_HEIGHT = 0x2;
+    const uint32_t DDSD_WIDTH = 0x4;
+    const uint32_t DDSD_PIXELFORMAT = 0x1000;
+    const uint32_t DDSD_LINEARSIZE = 0x80000;
+    const uint32_t DDPF_FOURCC = 0x4;
+    const uint32_t DDPF_RGB = 0x40;
+    const uint32_t DDPF_ALPHAPIXELS = 0x1;
+    const uint32_t DDSCAPS_TEXTURE = 0x1000;
+    const uint32_t DDSCAPS_MIPMAP = 0x400000;
+    const uint32_t DDPF_LUMINANCE = 0x20000;
+
+    // DDS Pixel Formats (FourCC Codes)
+    const uint32_t DXT1_FOURCC = MAKEFOURCC('D', 'X', 'T', '1');
+    const uint32_t DXT3_FOURCC = MAKEFOURCC('D', 'X', 'T', '3');
+    const uint32_t DXT5_FOURCC = MAKEFOURCC('D', 'X', 'T', '5');
+    const uint32_t DXN_FOURCC = MAKEFOURCC('A', 'T', 'I', '2'); // (DXN / BC5)
+
+    // Additional DDS Cubemap Flags
+    const uint32_t DDSCAPS2_CUBEMAP = 0x200;
+    const uint32_t DDSCAPS2_CUBEMAP_POSITIVEX = 0x400;
+    const uint32_t DDSCAPS2_CUBEMAP_NEGATIVEX = 0x800;
+    const uint32_t DDSCAPS2_CUBEMAP_POSITIVEY = 0x1000;
+    const uint32_t DDSCAPS2_CUBEMAP_NEGATIVEY = 0x2000;
+    const uint32_t DDSCAPS2_CUBEMAP_POSITIVEZ = 0x4000;
+    const uint32_t DDSCAPS2_CUBEMAP_NEGATIVEZ = 0x8000;
+
+    void GPUEndianSwapTexture(std::vector<uint8_t> &pixelData, GPUENDIAN endianType)
+    {
+        switch (endianType)
+        {
+        case GPUENDIAN_8IN16:
+            XGEndianSwapMemory(pixelData.data(), pixelData.data(), XGENDIAN_8IN16, 2, pixelData.size() / 2);
+            break;
+        case GPUENDIAN_8IN32:
+            XGEndianSwapMemory(pixelData.data(), pixelData.data(), XGENDIAN_8IN32, 4, pixelData.size() / 4);
+            break;
+        case GPUENDIAN_16IN32:
+            XGEndianSwapMemory(pixelData.data(), pixelData.data(), XGENDIAN_16IN32, 4, pixelData.size() / 4);
+            break;
+        }
+    }
+
+    void Dump_Image(const GfxImage *image)
+    {
+        if (!image)
+        {
+            Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Null GfxImage!\n");
+            return;
+        }
+
+        if (image->mapType != MAPTYPE_2D && image->mapType != MAPTYPE_CUBE)
+        {
+            Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Unsupported map type %d!\n", image->mapType);
+            return;
+        }
+
+        UINT BaseSize;
+        XGGetTextureLayout(image->texture.basemap, 0, &BaseSize, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        DDSHeader header;
+        memset(&header, 0, sizeof(DDSHeader));
+
+        header.magic = _byteswap_ulong(DDS_MAGIC);
+        header.size = _byteswap_ulong(DDS_HEADER_SIZE);
+        header.flags = _byteswap_ulong(DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH | DDSD_PIXELFORMAT | DDSD_LINEARSIZE);
+        header.height = _byteswap_ulong(image->height);
+        header.width = _byteswap_ulong(image->width);
+        header.depth = _byteswap_ulong(image->depth);
+        header.mipMapCount = _byteswap_ulong(image->texture.basemap->GetLevelCount());
+
+        auto format = image->texture.basemap->Format.DataFormat;
+        switch (format)
+        {
+        case GPUTEXTUREFORMAT_DXT1:
+            header.pixelFormat.fourCC = _byteswap_ulong(DXT1_FOURCC);
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_DXT2_3:
+            header.pixelFormat.fourCC = _byteswap_ulong(DXT3_FOURCC);
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_DXT4_5:
+            header.pixelFormat.fourCC = _byteswap_ulong(DXT5_FOURCC);
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_DXN:
+            header.pixelFormat.fourCC = _byteswap_ulong(DXN_FOURCC);
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_8:
+            header.pixelFormat.flags = _byteswap_ulong(DDPF_LUMINANCE);
+            header.pixelFormat.rgbBitCount = _byteswap_ulong(8);
+            header.pixelFormat.rBitMask = _byteswap_ulong(0x000000FF);
+            header.pixelFormat.gBitMask = 0;
+            header.pixelFormat.bBitMask = 0;
+            header.pixelFormat.aBitMask = 0;
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_8_8:
+            header.pixelFormat.flags = _byteswap_ulong(DDPF_LUMINANCE | DDPF_ALPHAPIXELS);
+            header.pixelFormat.rgbBitCount = _byteswap_ulong(16);
+            header.pixelFormat.rBitMask = _byteswap_ulong(0x000000FF);
+            header.pixelFormat.gBitMask = _byteswap_ulong(0x0000FF00);
+            header.pixelFormat.bBitMask = 0;
+            header.pixelFormat.aBitMask = 0;
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        case GPUTEXTUREFORMAT_8_8_8_8:
+            header.pixelFormat.flags = _byteswap_ulong(DDPF_RGB | DDPF_ALPHAPIXELS);
+            header.pixelFormat.rgbBitCount = _byteswap_ulong(32);
+            header.pixelFormat.rBitMask = _byteswap_ulong(0x00FF0000);
+            header.pixelFormat.gBitMask = _byteswap_ulong(0x0000FF00);
+            header.pixelFormat.bBitMask = _byteswap_ulong(0x000000FF);
+            header.pixelFormat.aBitMask = _byteswap_ulong(0xFF000000);
+            header.pitchOrLinearSize = BaseSize;
+            break;
+        default:
+            Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Unsupported texture format %d!\n", format);
+            return;
+        }
+
+        // Set texture capabilities
+        header.caps = _byteswap_ulong(DDSCAPS_TEXTURE | DDSCAPS_MIPMAP);
+
+        // Handle Cubemaps
+        if (image->mapType == MAPTYPE_CUBE)
+        {
+            header.caps2 = _byteswap_ulong(DDSCAPS2_CUBEMAP |
+                                           DDSCAPS2_CUBEMAP_POSITIVEX | DDSCAPS2_CUBEMAP_NEGATIVEX |
+                                           DDSCAPS2_CUBEMAP_POSITIVEY | DDSCAPS2_CUBEMAP_NEGATIVEY |
+                                           DDSCAPS2_CUBEMAP_POSITIVEZ | DDSCAPS2_CUBEMAP_NEGATIVEZ);
+        }
+
+        std::string filename = "game:\\_iw3xe\\dump\\images\\";
+        std::string sanitized_name = image->name;
+
+        // Remove invalid characters
+        sanitized_name.erase(std::remove_if(sanitized_name.begin(), sanitized_name.end(), [](char c)
+                                            { return c == '*'; }),
+                             sanitized_name.end());
+
+        filename += sanitized_name + ".dds";
+
+        std::ofstream file(filename, std::ios::binary);
+        if (!file)
+        {
+            Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Failed to open file: %s\n", filename.c_str());
+            return;
+        }
+
+        if (image->mapType == MAPTYPE_CUBE)
+        {
+            file.write(reinterpret_cast<const char *>(&header), sizeof(DDSHeader));
+
+            unsigned int face_size = 0;
+            unsigned int rowPitch = 0;
+            const GPUTEXTUREFORMAT format = static_cast<GPUTEXTUREFORMAT>(image->texture.basemap->Format.DataFormat);
+
+            switch (format)
+            {
+            case GPUTEXTUREFORMAT_DXT1:
+                face_size = (image->width / 4) * (image->height / 4) * 8;
+                rowPitch = (image->width / 4) * 8; // 8 bytes per 4x4 block
+                break;
+            case GPUTEXTUREFORMAT_8_8_8_8:
+                face_size = image->width * image->height * 4;
+                rowPitch = image->width * 4; // 4 bytes per pixel
+                break;
+            default:
+                Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Unsupported cube map format %d!\n", format);
+                return;
+            }
+
+            // TODO: handle mip levels per face for cubemaps
+            for (int i = 0; i < 6; i++)
+            {
+                unsigned char *face_pixels = image->pixels + (i * face_size); // Offset for each face
+
+                std::vector<uint8_t> swappedFace(face_pixels, face_pixels + face_size);
+                GPUEndianSwapTexture(swappedFace, static_cast<GPUENDIAN>(image->texture.basemap->Format.Endian));
+
+                // Create buffer for linear texture data
+                std::vector<uint8_t> linearFace(face_size);
+
+                // Convert tiled texture to linear layout using XGUntileTextureLevel
+                XGUntileTextureLevel(
+                    image->width,               // Width
+                    image->height,              // Height
+                    0,                          // Level (base level)
+                    static_cast<DWORD>(format), // GpuFormat
+                    0,                          // Flags (no special flags)
+                    linearFace.data(),          // pDestination (linear output)
+                    rowPitch,                   // RowPitch
+                    nullptr,                    // pPoint (no offset)
+                    swappedFace.data(),         // pSource (tiled input)
+                    nullptr                     // pRect (entire texture)
+                );
+
+                file.write(reinterpret_cast<const char *>(linearFace.data()), linearFace.size());
+            }
+
+            file.close();
+        }
+        else if (image->mapType == MAPTYPE_2D)
+        {
+            // TODO: write mip levels
+            file.write(reinterpret_cast<const char *>(&header), sizeof(DDSHeader));
+
+            std::vector<uint8_t> pixelData(image->pixels, image->pixels + image->baseSize);
+
+            GPUEndianSwapTexture(pixelData, static_cast<GPUENDIAN>(image->texture.basemap->Format.Endian));
+
+            // Create a linear data buffer to hold the untiled texture
+            std::vector<uint8_t> linearData(image->baseSize);
+
+            // Calculate row pitch based on format
+            UINT rowPitch;
+            auto format = image->texture.basemap->Format.DataFormat;
+
+            switch (format)
+            {
+            case GPUTEXTUREFORMAT_DXT1:
+            case GPUTEXTUREFORMAT_DXT2_3:
+            case GPUTEXTUREFORMAT_DXT4_5:
+            case GPUTEXTUREFORMAT_DXN:
+                // Block compressed formats use 4x4 blocks
+                rowPitch = ((image->width + 3) / 4) * (format == GPUTEXTUREFORMAT_DXT1 ? 8 : 16);
+                break;
+            case GPUTEXTUREFORMAT_8:
+                rowPitch = image->width;
+                break;
+            case GPUTEXTUREFORMAT_8_8:
+                rowPitch = image->width * 2;
+                break;
+            case GPUTEXTUREFORMAT_8_8_8_8:
+                rowPitch = image->width * 4;
+                break;
+            default:
+                Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Unsupported texture format %d!\n", format);
+                return;
+            }
+
+            xbox::DbgPrint("Image_Dump: rowPitch=%d\n", rowPitch);
+
+            // Call XGUntileTextureLevel to convert the tiled texture to linear format
+            XGUntileTextureLevel(
+                image->width,               // Width
+                image->height,              // Height
+                0,                          // Level (base level 0)
+                static_cast<DWORD>(format), // GpuFormat
+                XGTILE_NONPACKED,           // Flags (no special flags)
+                linearData.data(),          // pDestination
+                rowPitch,                   // RowPitch (calculated based on format)
+                nullptr,                    // pPoint (no offset)
+                pixelData.data(),           // pSource
+                nullptr                     // pRect (entire texture)
+            );
+
+            file.write(reinterpret_cast<const char *>(linearData.data()), linearData.size());
+
+            file.close();
+        }
+        else
+        {
+            Com_PrintError(CON_CHANNEL_ERROR, "Image_Dump: Unsupported map type %d!\n", image->mapType);
+            return;
+        }
+    }
+
+    void Cmd_dump_images_f()
+    {
+        CreateDirectoryA("game:\\_iw3xe", nullptr);
+        CreateDirectoryA("game:\\_iw3xe\\dump", nullptr);
+        CreateDirectoryA("game:\\_iw3xe\\dump\\images", nullptr);
+
+        const int MAX = 2048;
+        XAssetHeader assets[MAX];
+        int count = DB_GetAllXAssetOfType(ASSET_TYPE_IMAGE, assets, MAX);
+
+        for (int i = 0; i < count; i++)
+        {
+            Dump_Image(assets[i].image);
+        }
+        Com_Printf(CON_CHANNEL_CONSOLEONLY, "Dumped %d rimages to 'game:\\_iw3xe\\dump\\images'\n", count);
+    }
+
     // Detour CG_InitConsoleCommands_Detour;
 
     // void CG_InitConsoleCommands_Hook()
@@ -267,6 +645,9 @@ namespace iw3_328
         // xbox::DbgPrint("Initializing CG_InitConsoleCommands detour...\n");
         // CG_InitConsoleCommands_Detour = Detour(CG_InitConsoleCommands, CG_InitConsoleCommands_Hook);
         // CG_InitConsoleCommands_Detour.Install();
+
+        cmd_function_s *dump_images_VAR = new cmd_function_s;
+        Cmd_AddCommandInternal("dump_images", Cmd_dump_images_f, dump_images_VAR);
 
         cmd_function_s *dump_mapents_VAR = new cmd_function_s;
         Cmd_AddCommandInternal("dump_mapents", Cmd_dump_mapents_f, dump_mapents_VAR);
