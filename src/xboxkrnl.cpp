a778cf5ccad81ed753743890a1e910a4f41b4a1c
Michael Oliver <michael.oliver_@outlook.com>
2025-04-23 15:30:51 +0100

    feat: implement function to check for emulator environment

--- src/xboxkrnl.cpp
+++ src/xboxkrnl.cpp
@@ -5,6 +5,16 @@ namespace xbox
 {
     XNOTIFYQUEUEUI XNotifyQueueUI = nullptr;
 
+    bool InXenia()
+    {
+        // https://github.com/ClementDreptin/XexUtils/blob/ae8a8b832315678255c00d6a9b967a9136155503/src/Xam_.cpp#L168
+        // https://github.com/RBEnhanced/RB3Enhanced/blob/106c4290ee4fc2a91da65f4092a2b881971dc59c/source/xbox360.c#L16
+
+        void *xamFirstExport = ResolveFunction("xam.xex", 1);
+        // If Xam is not in the typical memory address space, we're in an emulator
+        return reinterpret_cast<uintptr_t>(xamFirstExport) >> 24 != 0x81;
+    }
+
     void *ResolveFunction(const std::string &moduleName, uint32_t ordinal)
     {
         HMODULE moduleHandle = GetModuleHandle(moduleName.c_str());
