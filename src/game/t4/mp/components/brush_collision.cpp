fc92242d4ab17fc09ee1b094595e7fcd465df0db
Michael Oliver <michael.oliver_@outlook.com>
2025-06-16 10:34:07 +0100

    fix(T4 MP): use correct struct for `dvar_s`

--- src/game/t4/mp/components/brush_collision.cpp
+++ src/game/t4/mp/components/brush_collision.cpp
@@ -5,6 +5,7 @@ namespace t4
 {
     namespace mp
     {
+        dvar_s *noclip_brushes = nullptr;
         std::vector<int> brushContents;
 
         void SaveBrushContents()
@@ -83,12 +84,11 @@ namespace t4
 
         void HandleBrushCollisionChange()
         {
-            static auto noclip_brushes = Dvar_FindMalleableVar("noclip_brushes");
             if (R_CheckDvarModified(noclip_brushes))
             {
                 CG_GameMessage(0, "noclip_brushes changed");
                 // TODO: find out why noclip_brushes->current.string isn't working
-                auto value = Dvar_GetString("noclip_brushes");
+                auto value = noclip_brushes->current.string;
 
                 // Empty string means reset to original contents
                 if (strcmp(value, "") == 0)
@@ -134,7 +134,7 @@ namespace t4
             DbgPrint("BrushCollision initialized\n");
             Sleep(1000); // Wait for the game to initialize
 
-            Dvar_RegisterString("noclip_brushes", "", DVAR_CODINFO, "Space separated list of brushes to disable collision on.");
+            noclip_brushes = Dvar_RegisterString("noclip_brushes", "", DVAR_CODINFO, "Space separated list of brushes to disable collision on.");
 
             CG_DrawActive_Detour = Detour(CG_DrawActive, CG_DrawActive_Hook);
             CG_DrawActive_Detour.Install();
