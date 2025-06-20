1c8d171ec22a122ecbd25c42826ac1b77acd438d
Michael Oliver <michael.oliver_@outlook.com>
2025-06-20 13:47:00 +0100

    feat(T4 SP): add player movement command GSC functions

--- src/game/t4/sp/symbols.h
+++ src/game/t4/sp/symbols.h
@@ -8,6 +8,8 @@ namespace t4
     {
         static auto g_entities = reinterpret_cast<gentity_s *>(0x8293A280);
 
+        static auto CL_CmdButtons = reinterpret_cast<void (*)(int a1, usercmd_s *a2)>(0x822E38F8);
+
         static auto Hunk_AllocateTempMemoryHighInternal = reinterpret_cast<void *(*)(int size)>(0x822862F8);
 
         static auto Scr_AddInt = reinterpret_cast<void (*)(int value, scriptInstance_t inst)>(0x82342000);
