d17aac892f681d8559fa45c46f5865f5a041eb7f
Michael Oliver <michael.oliver_@outlook.com>
2025-07-04 11:32:18 +0100

    feat(IW3 MP): implement component system

--- src/game/iw3/mp/main.h
+++ src/game/iw3/mp/main.h
@@ -4,8 +4,16 @@
 
 namespace iw3
 {
+    class Module
+    {
+    public:
+        Module() {};
+        virtual ~Module() {};
+        virtual const char *get_name() { return "unknown"; }
+    };
     namespace mp
     {
         void init();
+        void shutdown();
     }
 }
