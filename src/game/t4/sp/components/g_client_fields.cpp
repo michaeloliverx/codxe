8ba8b462d7286d388dd6181f1683c70e40604d2f
Michael Oliver <michael.oliver_@outlook.com>
2025-07-21 11:21:04 +0100

    feat(T4 SP): add `g_client_fields` module (#80)
    * feat(T4 SP): add g_client_fields module
    
    * update readme

---
+++ src/game/t4/sp/components/g_client_fields.cpp
@@ -0,0 +1,134 @@
+#include "g_client_fields.h"
+#include "common.h"
+
+namespace t4
+{
+    namespace sp
+    {
+        const int CLIENT_FIELD_MASK = 0x6000;
+
+        void ClientScr_SetGod(gclient_s *pSelf, const client_fields_s *field)
+        {
+            auto ent = &g_entities[pSelf - g_clients];
+
+            const auto value = Scr_GetInt(0, SCRIPTINSTANCE_SERVER);
+            if (value)
+            {
+                ent->flags |= 1; // Set the god mode flag
+            }
+            else
+            {
+                ent->flags &= ~1; // Clear the god mode flag
+            }
+        }
+
+        void ClientScr_GetGod(gclient_s *pSelf, const client_fields_s *field)
+        {
+            const auto ent = &g_entities[pSelf - g_clients];
+            const int value = (ent->flags & 1) ? 1 : 0;
+            Scr_AddInt(value, SCRIPTINSTANCE_SERVER);
+        }
+
+        // Updated fields array with named function pointers
+        const client_fields_s fields_extended[] =
+            {
+                // Original fields
+                {"playername", 0, F_LSTRING, 0x0, ClientScr_ReadOnly, ClientScr_GetName},
+                {"downs", 8392, F_INT, 0x0, NULL, NULL},
+                {"revives", 8396, F_INT, 0x0, NULL, NULL},
+                {"headshots", 8400, F_INT, 0x0, NULL, NULL},
+                {"nolean", 12, F_BITFLAG, 0x2000000, NULL, NULL},
+                {"nopronerotation", 12, F_BITFLAG, 0x10000000, NULL, NULL},
+                {"kills", 8384, F_INT, 0x0, NULL, NULL},
+                {"assists", 8388, F_INT, 0x0, NULL, NULL},
+                {"score", 8380, F_INT, 0x0, ClientScr_SetScore, NULL},
+                {"sessionstate", 0, F_STRING, 0x0, ClientScr_SetSessionState, ClientScr_GetSessionState},
+                {"statusicon", 0, F_STRING, 0x0, ClientScr_SetStatusIcon, ClientScr_GetStatusIcon},
+                {"spectatorclient", 8368, F_INT, 0x0, ClientScr_SetSpectatorClient, NULL},
+                {"archivetime", 8376, F_FLOAT, 0x0, ClientScr_SetArchiveTime, ClientScr_GetArchiveTime},
+                {"psoffsettime", 8760, F_INT, 0x0, ClientScr_SetPSOffsetTime, ClientScr_GetPSOffsetTime},
+                {"pers", 8412, F_OBJECT, 0x0, ClientScr_ReadOnly, NULL},
+                {"usingturret", 204, F_BITFLAG, 0x300, ClientScr_ReadOnly, NULL},
+                {"usingvehicle", 204, F_BITFLAG, 0x4000, ClientScr_ReadOnly, NULL},
+                {"vehicleposition", 2112, F_INT, 0x0, ClientScr_ReadOnly, NULL},
+                {"headicon", 0, F_STRING, 0x0, ClientScr_SetHeadIcon, ClientScr_GetHeadIcon},
+
+                // Custom fields
+                {"noclip", 8768, F_INT, 0x0, NULL, NULL},
+                {"ufo", 8772, F_INT, 0x0, NULL, NULL},
+                {"god", 0, F_INT, 0x0, ClientScr_SetGod, ClientScr_GetGod},
+                {NULL, 0, F_INT, 0x0, NULL, NULL}};
+
+        Detour GScr_AddFieldsForClient_Detour;
+
+        void GScr_AddFieldsForClient_Hook()
+        {
+            for (int i = 0; fields_extended[i].name != nullptr; i++)
+            {
+                Scr_AddClassField(0, fields_extended[i].name, static_cast<unsigned short>(i) | CLIENT_FIELD_MASK, SCRIPTINSTANCE_SERVER);
+                DbgPrint("Added client field: %s\n", fields_extended[i].name);
+            }
+        }
+
+        Detour Scr_SetClientField_Detour;
+
+        void Scr_SetClientField_Hook(gclient_s *client, unsigned int offset)
+        {
+            const auto field = &fields_extended[offset];
+            if (field->setter)
+                field->setter(client, field);
+            else
+                Scr_SetGenericField(reinterpret_cast<unsigned char *>(client), field->type, field->ofs, SCRIPTINSTANCE_SERVER);
+        }
+
+        void Scr_GetClientField(gclient_s *client, unsigned int offset)
+        {
+            const auto field = &fields_extended[offset];
+            if (field->getter)
+                field->getter(client, field);
+            else
+                Scr_GetGenericField(reinterpret_cast<unsigned char *>(client), field->type, field->ofs, SCRIPTINSTANCE_SERVER, field->whichbits);
+        }
+
+        // Retail TU7 SP has Scr_GetClientField and Scr_GetEntityField inlined, so we need to hook Scr_GetObjectField
+        // to handle client fields.
+        Detour Scr_GetObjectField_Detour;
+
+        void Scr_GetObjectField_Hook(unsigned int classnum, int entnum, int offset, scriptInstance_t inst)
+        {
+            // Mimic the client field conditions
+            if (classnum == 0 && (offset & CLIENT_FIELD_MASK) == CLIENT_FIELD_MASK)
+            {
+                // This is a client field
+                Scr_GetClientField(g_entities[entnum].client, offset & ~CLIENT_FIELD_MASK);
+                return;
+            }
+            else
+            {
+                // Call the original function for non-client fields
+                Scr_GetObjectField_Detour.GetOriginal<decltype(Scr_GetObjectField)>()(classnum, entnum, offset, inst);
+            }
+        }
+
+        g_client_fields::g_client_fields()
+        {
+            GScr_AddFieldsForClient_Detour = Detour(GScr_AddFieldsForClient, GScr_AddFieldsForClient_Hook);
+            GScr_AddFieldsForClient_Detour.Install();
+
+            Scr_SetClientField_Detour = Detour(Scr_SetClientField, Scr_SetClientField_Hook);
+            Scr_SetClientField_Detour.Install();
+
+            Scr_GetObjectField_Detour = Detour(Scr_GetObjectField, Scr_GetObjectField_Hook);
+            Scr_GetObjectField_Detour.Install();
+        }
+
+        g_client_fields::~g_client_fields()
+        {
+            GScr_AddFieldsForClient_Detour.Remove();
+
+            Scr_SetClientField_Detour.Remove();
+
+            Scr_GetObjectField_Detour.Remove();
+        }
+    }
+}
--- src/game/t4/sp/symbols.h
+++ src/game/t4/sp/symbols.h
@@ -6,22 +6,74 @@ namespace t4
 {
     namespace sp
     {
+        // Data
+        static auto g_clients = reinterpret_cast<gclient_s *>(0x82ABAC00);
         static auto g_entities = reinterpret_cast<gentity_s *>(0x8293A280);
+        static auto level = reinterpret_cast<level_locals_t *>(0x82AC3930);
 
+        const int BSP_ONLY_SPAWNS_COUNT = 16;
+        static auto s_bspOnlySpawns = reinterpret_cast<SpawnFuncEntry *>(0x8202CA40);
+
+        const int BSP_OR_DYNAMIC_SPAWNS_COUNT = 11;
+        static auto s_bspOrDynamicSpawns = reinterpret_cast<SpawnFuncEntry *>(0x8202CAC0);
+
+        // Null-terminated array of client fields
+        static auto client_fields = reinterpret_cast<client_fields_s *>(0x82019DD0);
+
+        // Functions
         static auto BG_AddPredictableEventToPlayerstate = reinterpret_cast<void (*)(unsigned __int8 newEvent, unsigned __int8 eventParm, struct playerState_s *ps)>(0x820EFC08);
 
         static auto CL_CmdButtons = reinterpret_cast<void (*)(int a1, usercmd_s *a2)>(0x822E38F8);
 
+        static auto ClientScr_ReadOnly = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC6F8);
+        static auto ClientScr_GetName = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC730);
+        static auto ClientScr_SetScore = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC938);
+        static auto ClientScr_SetSessionState = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC7A0);
+        static auto ClientScr_GetSessionState = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC8B0);
+        static auto ClientScr_SetStatusIcon = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCA40);
+        static auto ClientScr_GetStatusIcon = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCA80);
+        static auto ClientScr_SetSpectatorClient = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BC9D8);
+        static auto ClientScr_SetArchiveTime = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCBB0);
+        static auto ClientScr_GetArchiveTime = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCC00);
+        static auto ClientScr_SetPSOffsetTime = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCC48);
+        static auto ClientScr_GetPSOffsetTime = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCC80);
+        static auto ClientScr_SetHeadIcon = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCAD0);
+        static auto ClientScr_GetHeadIcon = reinterpret_cast<void (*)(gclient_s *pSelf, const client_fields_s *pField)>(0x821BCB30);
+
+        static auto Com_PrintError = reinterpret_cast<void (*)(int channel, const char *fmt, ...)>(0x8224F8C0);
+
         static auto Hunk_AllocateTempMemoryHighInternal = reinterpret_cast<void *(*)(int size)>(0x822862F8);
 
         static auto Scr_AddInt = reinterpret_cast<void (*)(int value, scriptInstance_t inst)>(0x82342000);
+        static auto Scr_GetInt = reinterpret_cast<int (*)(unsigned int index, scriptInstance_t inst)>(0x823479F8);
         static auto Scr_AddSourceBuffer = reinterpret_cast<char *(*)(scriptInstance_t inst, const char *filename, const char *extFilename, const char *codePos, bool archive)>(0x82334558);
         static auto Scr_Error = reinterpret_cast<void (*)(const char *error, scriptInstance_t inst)>(0x823489A8);
         static auto Scr_GetMethod = reinterpret_cast<BuiltinMethod (*)(const char **pName, int *type)>(0x821FE6F0);
         static auto Player_GetMethod = reinterpret_cast<BuiltinMethod (*)(const char **pName)>(0x821C39B8);
         static auto Scr_GetNumParam = reinterpret_cast<unsigned int (*)(scriptInstance_t inst)>(0x82341FE8);
         static auto Scr_ObjectError = reinterpret_cast<void (*)(const char *error, scriptInstance_t inst)>(0x823428F8);
+        static auto Scr_GetString = reinterpret_cast<const char *(*)(unsigned int index, scriptInstance_t inst)>(0x823480F0);
+        static auto Scr_ParamError = reinterpret_cast<void (*)(unsigned int index, const char *error, scriptInstance_t inst)>(0x82342878);
+        static auto GetObjectType = reinterpret_cast<unsigned int (*)(unsigned int id, scriptInstance_t inst)>(0x82339DC8);
+        static auto Scr_GetEntity = reinterpret_cast<gentity_s *(*)(unsigned int index)>(0x82211A08);
+
+        static auto GScr_AddFieldsForClient = reinterpret_cast<void (*)()>(0x821BCC90);
+        static auto Scr_AddClassField = reinterpret_cast<void (*)(unsigned int classnum, const char *name, unsigned __int16 offset, scriptInstance_t inst)>(0x8233DE38);
+
+        static auto Scr_SetGenericField = reinterpret_cast<void (*)(unsigned __int8 *b, fieldtype_t type, int ofs, scriptInstance_t inst)>(0x82212D38);
+        static auto Scr_GetGenericField = reinterpret_cast<void (*)(unsigned __int8 *b, fieldtype_t type, int ofs, scriptInstance_t inst, unsigned int whichbits)>(0x82212FA8);
+
+        static auto Scr_SetClientField = reinterpret_cast<void (*)(gclient_s *client, int offset, scriptInstance_t inst)>(0x821BCCF0);
+        static auto Scr_GetObjectField = reinterpret_cast<void (*)(unsigned int classnum, int entnum, int offset, scriptInstance_t inst)>(0x82213670);
+
+        static auto GScr_AllocString = reinterpret_cast<int (*)(const char *s)>(0x821E2CE0);
+
+        static auto SV_LinkEntity = reinterpret_cast<void (*)(gentity_s *gEnt)>(0x8225DB98);
+        static auto SV_UnlinkEntity = reinterpret_cast<void (*)(gentity_s *gEnt)>(0x8225DB50);
+        static auto SV_SetBrushModel = reinterpret_cast<void (*)(gentity_s *ent)>(0x8225CC20);
+        static auto SV_LocateGameData = reinterpret_cast<void (*)(gentity_s *gEnts, int numGEntities, int sizeofGEntity_t, playerState_s *clients, int sizeofGameClient)>(0x8225CCA8);
 
         static auto va = reinterpret_cast<char *(*)(char *format, ...)>(0x82294218);
+
     }
 }
