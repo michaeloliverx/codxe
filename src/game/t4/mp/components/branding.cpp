6d040c4e92e856a5e2d0df8fd52ea7441a19abf9
Michael Oliver <michael.oliver_@outlook.com>
2025-07-22 12:41:06 +0100

    refactor: centralise brand string building logic

--- src/game/t4/mp/components/branding.cpp
+++ src/game/t4/mp/components/branding.cpp
@@ -14,17 +14,12 @@ namespace t4
 
         void Branding::DrawBranding()
         {
-            const char *branding = "CoDxe T3 MP";
-            const char *build = __DATE__ " " __TIME__;
-            char brandingWithBuild[256];
-
-            _snprintf(brandingWithBuild, sizeof(brandingWithBuild), "%s (Build %d)", branding, BUILD_NUMBER);
+            const char *brandingWithBuild = branding::GetBrandingString(::branding::GAME_T4, ::branding::MODE_MP);
 
             static Font_s *font = (Font_s *)DB_FindXAssetHeader(ASSET_TYPE_FONT, "fonts/consoleFont", 1, -1);
 
             static float color[4] = {1.0, 1.0, 1.0, 0.6};
             R_AddCmdDrawText(brandingWithBuild, 256, font, 10, 20, 1, 1, 1, color, 0);
-            R_AddCmdDrawText(build, 256, font, 10, 40, 1.0, 1.0, 0.0, color, 0);
         }
 
         Branding::Branding()
