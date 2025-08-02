6d347243cdacd615cc0e5db0f5e23ebf8cb5f363
Michael Oliver <michael.oliver_@outlook.com>
2025-08-02 11:26:21 +0100

    remove cj experiments

--- src/game/qos/mp/components/g_scr_main.cpp
+++ src/game/qos/mp/components/g_scr_main.cpp
@@ -68,89 +68,6 @@ namespace qos
             return pMethod;
         }
 
-        void PlayerCmd_ToggleUFO(scr_entref_t entref)
-        {
-            if (entref.classnum != 0)
-                Scr_ObjectError("not an entity");
-
-            const gentity_s *ent = &g_entities[entref.entnum];
-            if (!ent->client)
-                Scr_ObjectError(va("entity %i is not a player", entref.entnum));
-
-            ent->client->ufo = !ent->client->ufo;
-        }
-
-        void PlayerCmd_GetUFO(scr_entref_t entref)
-        {
-            if (entref.classnum != 0)
-                Scr_ObjectError("not an entity");
-
-            const gentity_s *ent = &g_entities[entref.entnum];
-            if (!ent->client)
-                Scr_ObjectError(va("entity %i is not a player", entref.entnum));
-
-            Scr_AddInt(ent->client->ufo);
-        }
-
-        void PlayerCmd_ToggleGod(scr_entref_t entref)
-        {
-            if (entref.classnum != 0)
-                Scr_ObjectError("not an entity");
-
-            gentity_s *ent = &g_entities[entref.entnum];
-            if (!ent->client)
-                Scr_ObjectError(va("entity %i is not a player", entref.entnum));
-
-            const int fl_god = 1;
-
-            ent->flags ^= fl_god;
-        }
-
-        void DisableBrushCollision()
-        {
-            for (int i = 0; i < cm->numBrushes; ++i)
-            {
-                cm->brushes[i].contents &= ~0x10000; // Disable collision for all brushes
-            }
-        }
-
-        /**
-         * Checks if a 3D point is contained within an axis-aligned bounding box
-         */
-        bool IsPointInBounds(const float mins[3], const float maxs[3], const float point[3])
-        {
-            return (point[0] >= mins[0] && point[0] <= maxs[0]) &&
-                   (point[1] >= mins[1] && point[1] <= maxs[1]) &&
-                   (point[2] >= mins[2] && point[2] <= maxs[2]);
-        }
-
-        void GScr_DisableBrushCollisionContainingPoint()
-        {
-            DbgPrint("QOS SP: GScr_DisableBrushCollisionContainingPoint called\n");
-            float point[3] = {0};
-            Scr_GetVector(0, point);
-
-            DbgPrint("QOS SP: Point to check: (%f, %f, %f)\n", point[0], point[1], point[2]);
-
-            std::vector<int> brushIndices;
-            for (int i = 0; i < cm->numBrushes; ++i)
-            {
-                auto &brush = cm->brushes[i];
-                if (brush.contents & 0x10000 /* CONTENTS_PLAYERCLIP */ &&
-                    IsPointInBounds(brush.mins, brush.maxs, point))
-                {
-                    brushIndices.push_back(i);
-                }
-            }
-
-            DbgPrint("QOS SP: Found %zu brushes containing the point\n", brushIndices.size());
-
-            for (size_t i = 0; i < brushIndices.size(); ++i)
-            {
-                cm->brushes[brushIndices[i]].contents &= ~0x10000; // Disable collision
-            }
-        }
-
         g_scr_main::g_scr_main()
         {
             Scr_GetFunction_Detour = Detour(Scr_GetFunction, Scr_GetFunction_Hook);
@@ -158,15 +75,6 @@ namespace qos
 
             Scr_GetMethod_Detour = Detour(Scr_GetMethod, Scr_GetMethod_Hook);
             Scr_GetMethod_Detour.Install();
-
-            // Registering functions
-            // Scr_AddFunction("disablebrushcollision", DisableBrushCollision, 0);
-            Scr_AddFunction("disablebrushcollisioncontainingpoint", GScr_DisableBrushCollisionContainingPoint, 0);
-
-            // Registering methods
-            Scr_AddMethod("togglegod", PlayerCmd_ToggleGod, 0);
-            Scr_AddMethod("toggleufo", PlayerCmd_ToggleUFO, 0);
-            Scr_AddMethod("getufo", PlayerCmd_GetUFO, 0);
         }
 
         g_scr_main::~g_scr_main()
