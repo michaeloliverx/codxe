6aab244218cb3eee0556956faf8eab1290f52eee
Michael Oliver <michael.oliver_@outlook.com>
2026-06-05 10:40:03 +0100

    feat(T4 MP): nop fastfile sig check

--- src/game/t4/mp/components/patches.cpp
+++ src/game/t4/mp/components/patches.cpp
@@ -5,6 +5,18 @@ namespace t4
 {
 namespace mp
 {
+
+void DisableFastfileRsaChecks()
+{
+    // DBX_AuthLoad_ValidateHash
+    *(volatile uint32_t *)0x821D7E9C = 0x60000000;
+    *(volatile uint32_t *)0x821D7F94 = 0x60000000;
+    *(volatile uint32_t *)0x821D7FE4 = 0x60000000;
+
+    // DBX_AuthLoad_ValidateSignature
+    *(volatile uint32_t *)0x821D7EB4 = 0x60000000;
+}
+
 void EnableExplosiveKnockback()
 {
     // sub_8220D2D0
@@ -21,6 +33,7 @@ void EnableRocketJumping()
 
 Patches::Patches()
 {
+    DisableFastfileRsaChecks();
     EnableExplosiveKnockback();
     EnableRocketJumping();
 }
