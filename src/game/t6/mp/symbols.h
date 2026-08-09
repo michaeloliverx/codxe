0aa7b40c321fbd95dd7e8374c2e1ead7f4c49505
Michael Oliver <michael.oliver_@outlook.com>
2026-08-09 20:31:38 +0100

    chore: clang format

--- src/game/t6/mp/symbols.h
+++ src/game/t6/mp/symbols.h
@@ -4,20 +4,22 @@
 
 namespace t6
 {
-    namespace mp
-    {
-        static auto Hunk_AllocateTempMemoryHighInternal = reinterpret_cast<void *(*)(int size)>(0x8248F1B0);
+namespace mp
+{
+static auto Hunk_AllocateTempMemoryHighInternal = reinterpret_cast<void *(*)(int size)>(0x8248F1B0);
 
-        static auto Scr_AddSourceBuffer = reinterpret_cast<char *(*)(scriptInstance_t inst, const char *filename, const char *extFilename, const char *codePos, bool archive)>(0x82530128);
+static auto Scr_AddSourceBuffer =
+    reinterpret_cast<char *(*)(scriptInstance_t inst, const char *filename, const char *extFilename,
+                               const char *codePos, bool archive)>(0x82530128);
 
-        struct ScriptParseTree
-        {
-            const char *name;
-            int len;
-            char *buffer;
-        };
+struct ScriptParseTree
+{
+    const char *name;
+    int len;
+    char *buffer;
+};
 
-        static auto Load_ScriptParseTreeAsset = reinterpret_cast<void (*)(ScriptParseTree **a1)>(0x822CA4C8);
+static auto Load_ScriptParseTreeAsset = reinterpret_cast<void (*)(ScriptParseTree **a1)>(0x822CA4C8);
 
-    }
-}
+} // namespace mp
+} // namespace t6
--- src/game/t6/sp/structs.h
+++ src/game/t6/sp/structs.h
@@ -2,12 +2,12 @@
 
 namespace t6
 {
-    namespace sp
-    {
-        enum scriptInstance_t : __int32
-        {
-            SCRIPTINSTANCE_SERVER = 0x0,
-            SCRIPTINSTANCE_CLIENT = 0x1,
-        };
-    }
-}
+namespace sp
+{
+enum scriptInstance_t : __int32
+{
+    SCRIPTINSTANCE_SERVER = 0x0,
+    SCRIPTINSTANCE_CLIENT = 0x1,
+};
+} // namespace sp
+} // namespace t6
